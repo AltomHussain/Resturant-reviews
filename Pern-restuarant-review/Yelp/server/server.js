c2e0523427be774ec773b92ac4500af100cde7c8
altom <altomhussian2001@yahoo.com>
2021-01-13 23:34:57 +0000

    Added middleware

--- Pern-restuarant-review/Yelp/server/server.js
+++ Pern-restuarant-review/Yelp/server/server.js
@@ -2,26 +2,34 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
-
+app.use((req, res, next)=>{
+    console.log("middleware");
+    next();
+})
 //retrieve
 app.get("/restaurants", (req, res)=>{
 console.log("restaurant");
 res.json("hello there")
 })
+//retrieve one restaurant
+app.get("/restaurants/:id", (req, res)=>{
+res.json("get one")
+
+})
 //create
 app.post("/restaurants", (req, res)=>{
-    
+    res.json("post")
 })
 
 //update
-app.put("/restaurants", (req, res)=>{
-
+app.put("/restaurants/:id", (req, res)=>{
+res.json("Update")
 })
 
 
 //delete
-app.delete("/restaurants", (req, res)=>{
-
+app.delete("/restaurants/:id", (req, res)=>{
+res.json("deleted")
 })
 
 
