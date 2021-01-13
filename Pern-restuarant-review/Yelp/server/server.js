d0f3e9f269458239babee26988ae05d04802b8c8
altom <altomhussian2001@yahoo.com>
2021-01-13 23:15:51 +0000

    added env file

--- Pern-restuarant-review/Yelp/server/server.js
+++ Pern-restuarant-review/Yelp/server/server.js
@@ -1,7 +1,32 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 
 
-app.listen(5000, ()=>{
-    console.log("Server is listening on port 5000");
+//retrieve
+app.get("/restaurants", (req, res)=>{
+console.log("restaurant");
+res.json("hello there")
+})
+//create
+app.post("/restaurants", (req, res)=>{
+    
+})
+
+//update
+app.put("/restaurants", (req, res)=>{
+
+})
+
+
+//delete
+app.delete("/restaurants", (req, res)=>{
+
+})
+
+
+
+const port = process.env.PORT ||4001;
+app.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`);
 })
