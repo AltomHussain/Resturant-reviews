4931f5fd2b718d0b56367b50c6b812641b31b3a2
altom <altomhussian2001@yahoo.com>
2021-01-14 18:52:15 +0000

    added get all restaurants and get one restaurant endpoint

--- Pern-restuarant-review/Yelp/server/db.sql
+++ Pern-restuarant-review/Yelp/server/db.sql
@@ -3,7 +3,7 @@ CREATE TABLE restaurants(
     id serial primary key,
     name VARCHAR(50) NOT NULL,
     location VARCHAR(100) NOT NULL,
-    price_range INT NOT NULL CHECK(price_range>= 1 AND price_range<=5)
+    price_range INT NOT NULL CHECK(price_range>= 1 AND price_range <=5)
 );
 
 INSERT INTO restaurants(name, location, price_range) VALUES( 'Best dinner', 'Manchester',4);
--- Pern-restuarant-review/Yelp/server/server.js
+++ Pern-restuarant-review/Yelp/server/server.js
@@ -1,40 +1,83 @@
 const express = require("express");
+const morgan = require("morgan");
+const db = require("./db");
 require("dotenv").config();
 const app = express();
+//req.body
+app.use(express.json());
+//this is a midleware
+app.use((req, res, next) => {
+  console.log("middleware");
+  next();
+});
+//added morgan middleware library to use it as middleware(it return the req path every time you send req)
+app.use(morgan("dev"));
 
-app.use((req, res, next)=>{
-    console.log("middleware");
-    next();
-})
 //retrieve
-app.get("/restaurants", (req, res)=>{
-console.log("restaurant");
-res.json("hello there")
-})
-//retrieve one restaurant
-app.get("/restaurants/:id", (req, res)=>{
-res.json("get one")
+app.get("/restaurants", async (req, res) => {
+    try {
+        const results = await db.query("select  * from restaurants");
+        res.status(200).json({
+            status: "success",
+            total: results.rows.length,
+            data: {
+                restaurant: results.rows
+            }
+                });
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
-})
+//retrieve one restaurant
+app.get("/restaurants/:id", async(req, res) => {
+  const { id } = req.params;
+try {
+    const results = await db.query("select * from restaurants where id= $1", [id]);
+    if(results.rows.length){
+        res.json({
+      status: "success",
+      total: results.rows.length,
+      data: {
+          restaurants: results.rows[0] 
+      }
+  });
+    }else{
+        res.status(404).json(`There is no restaurant with that id: ${id}`)
+    }
+    
+  console.log();
+} catch (error) {
+    console.log(error.message);
+}
+  
+});
+
 //create
-app.post("/restaurants", (req, res)=>{
-    res.json("post")
-})
+app.post("/restaurants", (req, res) => {
+  res.status(201).json({
+    status: "success",
+  });
+});
 
 //update
-app.put("/restaurants/:id", (req, res)=>{
-res.json("Update")
-})
-
+app.put("/restaurants/:id", (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({
+    status: "success",
+  });
+});
 
 //delete
-app.delete("/restaurants/:id", (req, res)=>{
-res.json("deleted")
-})
+app.delete("/restaurants/:id", (req, res) => {
+  const { id } = req.params;
+  res.status(204).json({
+    status: "success",
+  });
+});
 
-
-
-const port = process.env.PORT ||4001;
-app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}`);
-})
+const port = process.env.PORT || 4001;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
