df631759e0f6165fa2d39395dfb874570fe8c1fa
altom <altomhussian2001@yahoo.com>
2021-01-16 21:01:54 +0000

    Finished building all endpoints

--- Pern-restuarant-review/Yelp/server/server.js
+++ Pern-restuarant-review/Yelp/server/server.js
@@ -15,66 +15,103 @@ app.use(morgan("dev"));
 
 //retrieve
 app.get("/restaurants", async (req, res) => {
-    try {
-        const results = await db.query("select  * from restaurants");
-        res.status(200).json({
-            status: "success",
-            total: results.rows.length,
-            data: {
-                restaurant: results.rows
-            }
-                });
-        
-    } catch (error) {
-        console.log(error.message);
-    }
-});
-
-//retrieve one restaurant
-app.get("/restaurants/:id", async(req, res) => {
-  const { id } = req.params;
-try {
-    const results = await db.query("select * from restaurants where id= $1", [id]);
-    if(results.rows.length){
-        res.json({
+  try {
+    const results = await db.query("select  * from restaurants");
+    res.status(200).json({
       status: "success",
       total: results.rows.length,
       data: {
-          restaurants: results.rows[0] 
-      }
-  });
-    }else{
-        res.status(404).json(`There is no restaurant with that id: ${id}`)
-    }
-    
-  console.log();
-} catch (error) {
+        restaurant: results.rows,
+      },
+    });
+  } catch (error) {
     console.log(error.message);
-}
-  
+  }
+});
+
+//retrieve one restaurant
+app.get("/restaurants/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const results = await db.query("select * from restaurants where id= $1", [
+      id,
+    ]);
+    if (results.rows.length) {
+      res.json({
+        status: "success",
+        total: results.rows.length,
+        data: {
+          restaurants: results.rows[0],
+        },
+      });
+    } else {
+      res.status(404).json(`There is no restaurant with that id: ${id}`);
+    }
+
+    console.log();
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 //create
-app.post("/restaurants", (req, res) => {
-  res.status(201).json({
-    status: "success",
-  });
+app.post("/restaurants", async (req, res) => {
+  try {
+    const { name, location, price_range } = req.body;
+    const results = await db.query(
+      "insert into restaurants (name, location, price_range) values($1, $2, $3) returning *",
+      [name, location, price_range]
+    );
+    if(name !== "" && location !=="" && price_range !==""){
+       res.status(201).json({
+      status: "success",
+      total: results.rows.length,
+      data: "your data has been inserted successfully",
+    }); 
+    }else{
+        res.status(401).json("Missing Credential")
+    }
+    
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 //update
-app.put("/restaurants/:id", (req, res) => {
+app.put("/restaurants/:id", async(req, res) => {
   const { id } = req.params;
-  res.status(200).json({
-    status: "success",
-  });
+  const { name, location, price_range } = req.body;
+  try {
+      const results = await db.query("update restaurants set name=$1,location=$2, price_range=$3 where id=$4 returning *", [name, location, price_range, id])
+      
+      if(results.rows.length){
+          res.status(200).json({
+            status: "success",
+            data:{
+                results: results.rows[0]
+            }
+          });
+      }else{
+          res.status(404).json(`Row with the id: ${id} does not exist`)
+      }
+  } catch (error) {
+      console.log(error.message);
+  }
 });
 
 //delete
-app.delete("/restaurants/:id", (req, res) => {
+app.delete("/restaurants/:id", async(req, res) => {
   const { id } = req.params;
-  res.status(204).json({
-    status: "success",
-  });
+  console.log(id);
+  const results = await db.query("delete from restaurants where id=$1 returning *", [id]);
+  if(results.rows.length !==0){
+      res.status(200).json({
+        status: "success",
+        data: `The row with id: ${id} has been deleted :)`
+      });
+  }else{
+res.status(404).json(`The row with id: ${id} does not exist`)
+  }
 });
 
 const port = process.env.PORT || 4001;
