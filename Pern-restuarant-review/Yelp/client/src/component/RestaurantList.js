6d25d5f0ace3bd5af5d4e1214e7ee16d1c8184bd
altom <altomhussian2001@yahoo.com>
2021-01-20 23:18:42 +0000

    Displayed all data after fetching

--- Pern-restuarant-review/Yelp/client/src/component/RestaurantList.js
+++ Pern-restuarant-review/Yelp/client/src/component/RestaurantList.js
@@ -1,48 +1,54 @@
-import React, {useContext, useEffect} from 'react'
-import RestaurantFinder from "../apis/RestaurantFinder"
-import { RestaurantContext } from '../contentex/RestaurantContext';
+import React, { useContext, useEffect } from "react";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantContext } from "../contentex/RestaurantContext";
 
 export default function RestaurantList(props) {
-const {restaurants, setRestaurants}= useContext(RestaurantContext )
-useEffect( ()=>{
- async function fetchData (){
-try {
- const res = await RestaurantFinder.get("/");
- setRestaurants(res.data.data.restaurants)
- console.log(restaurants);
-} catch (error) {
-  console.log(error.message);
-}
- }
-fetchData()
-}, [])
+  const { restaurants, setRestaurants } = useContext(RestaurantContext);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await RestaurantFinder.get("/");
+        setRestaurants(res.data.data.restaurants);
+        console.log(restaurants);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchData();
+  }, []);
 
-    return (
-        <div className="list-group">
-            <table class="table table-hover table-dark">
-  <thead>
-    <tr className="bg-primary">
-      <th scope="col">Restaurant</th>
-      <th scope="col">Location</th>
-      <th scope="col">Price Range</th>
-      <th scope="col">Rating</th>
-      <th scope="col">Edit</th>
-      <th scope="col">Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      {/* <th scope="row">1</th> */}
-      <td>McDonald</td>
-      <td>Manchester</td>
-      <td>$$</td>
-      <td>Rating</td>
-      <td><button className="btn btn-warning">Edit</button> </td>
-      <td><button className="btn btn-danger">Delete</button> </td>
-    </tr>
-   
-  </tbody>
-</table>
-        </div>
-    )
+  return (
+    <div className="list-group">
+      <table class="table table-hover table-dark">
+        <thead>
+          <tr className="bg-primary">
+            <th scope="col">Restaurant</th>
+            <th scope="col">Location</th>
+            <th scope="col">Price Range</th>
+            <th scope="col">Rating</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {restaurants && restaurants.map(({ name, location, price_range,id  }) => {
+            return (
+              <tr key={id}>
+                <td>{name}</td>
+                <td>{location}</td>
+                <td>{"$".repeat(price_range)}</td>
+                <td>Review</td>
+                <td>
+                  <button className="btn btn-warning">Edit</button>{" "}
+                </td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>{" "}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
