d92093692cf5e7a72630acc524de4b068fee73d1
altom <altomhussian2001@yahoo.com>
2021-01-24 23:00:54 +0000

    added review table

--- Pern-restuarant-review/Yelp/server/db.sql
+++ Pern-restuarant-review/Yelp/server/db.sql
@@ -6,4 +6,15 @@ CREATE TABLE restaurants(
     price_range INT NOT NULL CHECK(price_range>= 1 AND price_range <=5)
 );
 
+CREATE TABLE reviews (
+id serial NOT Null primary key ,
+restaurant_id INT NOT null REFERENCES restaurants(id),
+name VARCHAR(50) NOT NULL,
+review Text NOT null,
+rating INT NOT null CHECK(rating>= 1 and rating<=5)
+);
+
 INSERT INTO restaurants(name, location, price_range) VALUES( 'Best dinner', 'Manchester',4);
+
+
+INSERT INTO reviews(name, restaurant_id, review, rating) VALUES('Altom',4, 'nice', 3);
