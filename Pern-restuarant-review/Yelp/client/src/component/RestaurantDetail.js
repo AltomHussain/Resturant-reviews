7dff0ff8af4464c92de278dfa1e797ae069f8c11
altom <altomhussian2001@yahoo.com>
2021-01-24 22:29:11 +0000

    Set up the restaurant details component

--- Pern-restuarant-review/Yelp/client/src/component/RestaurantDetail.js
+++ Pern-restuarant-review/Yelp/client/src/component/RestaurantDetail.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantContext } from "../contentex/RestaurantContext";
 
 export default function RestaurantDetail() {
-    return (
-        <div>
-            detail
-        </div>
-    )
+  const { id } = useParams();
+  const { selectedRestaurant, setSelectedRestaurant } = useContext(
+    RestaurantContext
+  );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await RestaurantFinder.get(`/${id}`);
+        setSelectedRestaurant(res.data.data.restaurants);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+ 
+  return <div>{ selectedRestaurant &&selectedRestaurant.name}</div>;
 }
